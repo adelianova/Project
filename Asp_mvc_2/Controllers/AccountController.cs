d608b81ba6b38a02ffaccf0e96de5f2a0ceb8e60
Adelia Nova <9a.adelianova@gmail.com>
2017-02-07 07:26:31 +0700

    modul2

--- Asp_mvc_2/Controllers/AccountController.cs
+++ Asp_mvc_2/Controllers/AccountController.cs
@@ -29,5 +29,32 @@ namespace Asp_mvc_2.Controllers
             }
             return View(); 
         }
+        public ActionResult Login() { 
+            return View(); 
+        } 
+        [HttpPost]
+        public ActionResult Login(UserLoginView ULV, string returnUrl) {
+            if (ModelState.IsValid) {
+                UserManager UM = new UserManager(); 
+                string password = UM.GetUserPassword(ULV.LoginName);
+                if (string.IsNullOrEmpty(password)) 
+                    ModelState.AddModelError("", "The user login or password provided is incorrect."); 
+                else { 
+                    if (ULV.Password.Equals(password)) { 
+                        FormsAuthentication.SetAuthCookie(ULV.LoginName, false);
+                        return RedirectToAction("Welcome", "Home");
+                    } 
+                    else {
+                        ModelState.AddModelError("", "The password provided is incorrect."); 
+                    } 
+                }
+            }
+            // If we got this far, something failed, redisplay form 
+            return View(ULV); 
+        } 
+        [Authorize] 
+        public ActionResult SignOut() { 
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Home"); }
     }
 }
